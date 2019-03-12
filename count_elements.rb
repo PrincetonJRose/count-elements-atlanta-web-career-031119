171da0b3e6458817390d1510c754a1ebf2c57b31
Princeton Rose <decoyzpr@gmail.com>
2019-03-12 13:07:10 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,27 @@
-def count_elements(array)
+def count_elements(element)
   # code goes here
+  counts = []
+  counting = []
+  final_count = {}
+  for item in element
+    if counting.include?(item)
+      next
+    else
+      counting << item
+    end
+  end
+  for item in counting
+    num = 0
+    for key in element
+      if item == key
+        num += 1
+      end
+    end
+    counts << num
+  end
+  num = -1
+  counting.each do |key|
+    final_count[key] = counts[num += 1]
+  end
+  return final_count
 end
- 
